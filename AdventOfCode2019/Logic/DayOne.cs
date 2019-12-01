a4dac77fbd399f5c1864c4924e87f8c34c7be8fd
Frida Gidlund <frida_gidde95@hotmail.com>
2019-12-01 22:30:53 +0100

    Add project files.

---
+++ AdventOfCode2019/Logic/DayOne.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AdventOfCode2019.Logic
+{
+    public static class DayOne
+    {
+        public static int CalculateAllFuelModules(int[] massInputs)
+        {
+            int totalFuel = 0;
+            for (int i = 0; i < massInputs.Length; i++)
+            {
+                //totalFuel += CalculateFuel(massInputs[i]);
+                totalFuel += RecursiveCalculateFuel(massInputs[i]);
+            }
+            return totalFuel;
+        }
+
+        private static int CalculateFuel(double mass)
+        {
+            try
+            {
+                return Convert.ToInt32(Math.Floor(mass / 3.0) - 2);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        private static int RecursiveCalculateFuel(int mass)
+        {
+            mass = Convert.ToInt32(Math.Floor(mass / 3.0) - 2);
+            if (mass < 0) return 0;
+            return mass += RecursiveCalculateFuel(mass);
+        }
+    }
+}
