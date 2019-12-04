b1bc3abce2c275150751e3651815dba16015ac41
Frida Gidlund <frida_gidde95@hotmail.com>
2019-12-04 18:59:13 +0100

    Day 3.1 complete

--- AdventOfCode2019/Logic/Day3.cs
+++ AdventOfCode2019/Logic/Day3.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -8,63 +7,71 @@ namespace AdventOfCode2019.Logic
 {
     public static class Day3
     {
+        struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
         public static string CrossTheStreams()
         {
             var massInputs = Helper.FileHandler.GetDoubleInput("input_03");
+            var wire1Positions = PerformAllSteps(massInputs.Item1);
+            var wire2Positions = PerformAllSteps(massInputs.Item2);
 
-            var wire1 = new Point(0, 0);
-            var wire2 = new Point(0, 0);
-
-            var wire1Positions = new List<Point>();
-            var wire2Positions = new List<Point>();
-
-            for (int i = 0; i < massInputs.Item1.Length; i++)
-            {
-                wire1 = DoStep(wire1, massInputs.Item1[i]);
-                wire1Positions.Add(wire1);
-
-                wire2 = DoStep(wire2, massInputs.Item2[i]);
-                wire2Positions.Add(wire2);
-            }
-
-            //wire1Positions = new List<Point>() { 
-            //    new Point(1, 2), 
-            //    new Point(2, 3),
-            //    new Point(62, 1),
-            //    new Point(-12,-13)
-            //};
-
-            //wire2Positions = new List<Point>() {
-            //    new Point(0, 12),
-            //    new Point(3, 3),
-            //    new Point(62, 1),
-            //    new Point(-11,-13)
-            //};
-
-            var x = wire1Positions.Intersect(wire2Positions).ToList(); ;
-
-            return "";
+            return CalculateClosestIntersection(wire1Positions.Intersect(wire2Positions).ToList());
         }
 
-        private static Point DoStep(Point position, string move)
+        private static List<Point> PerformAllSteps(string[] inputs)
         {
-            var steps = Int32.Parse(move.Substring(1));
-            switch (move[0])
+            var wire = new Point();
+            var wirePositions = new List<Point>();
+
+            for (int i = 0; i < inputs.Length; i++)
             {
-                case 'U':
-                    position.Y += steps;
-                    break;
-                case 'D':
-                    position.Y -= steps;
-                    break;
-                case 'R':
-                    position.X += steps;
-                    break;
-                case 'L':
-                    position.X -= steps;
-                    break;
+                var range = DoStep(wire, inputs[i]);
+                wirePositions.AddRange(range);
+                wire = wirePositions.Last();
             }
-            return position;
+            return wirePositions;
+        }
+
+        private static List<Point> DoStep(Point position, string move)
+        {
+            List<Point> positions = new List<Point>();
+            var steps = Int32.Parse(move.Substring(1));
+            for (int i = 0; i < steps; i++)
+            {
+                switch (move[0])
+                {
+                    case 'U':
+                        position.Y++;
+                        break;
+                    case 'D':
+                        position.Y--;
+                        break;
+                    case 'R':
+                        position.X++;
+                        break;
+                    case 'L':
+                        position.X--;
+                        break;
+                }
+                positions.Add(position);
+            }
+            return positions;
+        }
+
+        private static string CalculateClosestIntersection(List<Point> intersectingWire)
+        {
+            int closest = int.MaxValue;
+
+            foreach (Point position in intersectingWire)
+            {
+                int distance = Math.Abs(position.X) + Math.Abs(position.Y);
+                if (distance < closest) closest = distance;
+            }
+            return closest.ToString();
         }
     }
 }
