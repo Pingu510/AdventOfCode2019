9dbf8c23eb2167d8aea876db4fc1e6acea51e16d
Frida Gidlund <frida_gidde95@hotmail.com>
2019-12-15 16:00:19 +0100

    day 6 complete

--- AdventOfCode2019/Logic/Day6.cs
+++ AdventOfCode2019/Logic/Day6.cs
@@ -10,8 +10,7 @@ namespace AdventOfCode2019.Logic
         public static (int,int) CalculateTravelPaths()
         {
             var planets = new List<Planet>();
-            //string[] orbits = Helper.FileHandler.GetPerRow("input_06");
-            string[] orbits = { "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN" };
+            string[] orbits = Helper.FileHandler.GetPerRow("input_06");
 
             int totalOrbits = TotalOrbits(planets, orbits);
             int minTravelPath = CalculateMinDistanceToSanta(planets);
@@ -88,11 +87,47 @@ namespace AdventOfCode2019.Logic
     
         private static int CalculateMinDistanceToSanta(List<Planet> planets)
         {
-
-
-            throw new NotImplementedException();
+            var youParent = planets.FirstOrDefault(x => x.Name == "YOU").Parent;
+            var sanParent = planets.FirstOrDefault(x => x.Name == "SAN").Parent;
+            var roots = FindCommonRoots(youParent, sanParent);
+            var minPath = FindSmallestRoot(roots, youParent, sanParent);
+            return minPath;
         }
 
+        private static List<Planet> FindCommonRoots(Planet youP, Planet sanP)
+        {
+            var youOrbits = FindAllOrbitsFor(youP);
+            var sanOrbits = FindAllOrbitsFor(sanP);
+            return youOrbits.Intersect(sanOrbits).ToList();
+        }
+
+        private static List<Planet> FindAllOrbitsFor(Planet planet)
+        {
+            var orbits = new List<Planet>();
+            Planet orbit = planet;
+            while (!orbit.IsRoot)
+            {
+                orbits.Add(orbit);
+                orbit = orbit.Parent;
+            }
+            return orbits;
+        }
+
+        private static int FindSmallestRoot(List<Planet> roots, Planet youP, Planet sanP)
+        {
+            int minPath = Int32.MaxValue;
+            foreach (var root in roots)
+            {
+                root.IsRoot = true;
+                CountOrbits(youP);
+                CountOrbits(sanP);
+                if (minPath > (youP.OrbitsTotals + sanP.OrbitsTotals))
+                {
+                    minPath = youP.OrbitsTotals + sanP.OrbitsTotals;
+                }
+            }
+            return minPath;
+        }
     }
 
     public class Planet
